b6b5e1f9dd447fc9a3178e2afbc1fe97982ae572
Anupkumr Seth <anupseth11@yahoo.com>
2021-11-07 14:46:35 +0530

    Implemented client side load balancing

--- currency-conversion-service/src/main/java/com/in28mins/microservices/currencyconversionservice/openfeign/CurrencyExchangeProxcy.java
+++ currency-conversion-service/src/main/java/com/in28mins/microservices/currencyconversionservice/openfeign/CurrencyExchangeProxcy.java
@@ -7,8 +7,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.in28mins.microservices.currencyconversionservice.beans.CurrencyConversion;
 
 // Enable the feign by adding annotation in main class
+
+
 // use the application name of the MS that you want to call. 
-@FeignClient(name = "currency-exchange",url = "localhost:8000") 
+// If we are not using naming server then we can just use this annotation 
+//@FeignClient(name = "currency-exchange",url = "localhost:8000") 
+
+// when we can are using naming server and we want to do load balancing then we can do this.
+// So in this MS there is a load balancer component (spring-cloud-loadbalancer) which is talking to naming server and doing Load balancing. 
+// This is called client side load balancing.
+@FeignClient(name = "currency-exchange") 
 public interface CurrencyExchangeProxcy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
